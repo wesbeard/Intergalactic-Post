f291e0bec1275f7f9acee0fafeba9ed9532082f1
Wes Beard <wesley.beard@mymail.champlain.edu>
2021-02-27 15:50:19 -0500

    Added modularity to the HTML via JS for each location

---
+++ Crash-site.js
@@ -0,0 +1,37 @@
+import {addTextItem, addEventButton, setArtwork} from "./scripts.js"
+
+class Crash_Site {
+    loadLocation(){
+        this.setText();
+        this.setEventButtons();
+        //this.setLocationArtwork();
+    }
+
+    setText(){
+        addTextItem("The warning alarm is blairing in your ear", true);
+        addTextItem("Please, shut up");
+        addTextItem("Red lights blink accross the control board", true);
+        addTextItem("That's not good");
+        addTextItem("\"Ship integrity: critical\"", true);
+        addTextItem("\"Vitals: critical\"", true);
+        addTextItem("\"Chance of survival: low\"", true);
+        addTextItem("That was the ship's computer, SAL");
+        addTextItem("He wasn't helping");
+        addTextItem("My body feels like it's been hit by a star freighter");
+        addTextItem("Suddenly, you come to your senses", true);
+        addTextItem("Oh god, the mail");
+        addTextItem("I need to deliver the mail");
+    }
+
+    setEventButtons() {
+        addEventButton("Look for the mail", "buttonFunction()")
+    }
+
+    setLocationArtwork() {
+        setArtwork(
+            
+        )
+    }
+}
+
+export {Crash_Site}
