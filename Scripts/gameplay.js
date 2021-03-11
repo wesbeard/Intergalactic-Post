5f949db026a15d6a121d6a9519787067583e1cbc
AdamHasTheBeans <adam.currier@mymail.champlain.edu>
2021-03-11 11:27:17 -0500

    got code running

--- Scripts/gameplay.js
+++ Scripts/gameplay.js
@@ -3,23 +3,22 @@ import {Resource_Manager, items} from "./ResourceManager.js";
 
 
 var _ResourceManager = new Resource_Manager();
-var _DisplayManager = new Display_Manager();
 // Add item to player inventory per click
 function clickAccumulate(type) {
    switch(type) {
        case 'b1':
            _ResourceManager.addItem(items.SCRAP_METAL, 5);
-           _DisplayManager.updateInventory(_ResourceManager);
+           Display_Manager.updateInventory(_ResourceManager);
            alert("here");
            break;
         case 'b2':
             _ResourceManager.addItem(items.WIRING, 1);
-            _DisplayManager.updateInventory(_ResourceManager);
+            Display_Manager.updateInventory(_ResourceManager);
             alert("here 2");
             break;
         case 'b3':
             _ResourceManager.addItem(items.MECHANICAL_PARTS, 2);
-            _DisplayManager.updateInventory(_ResourceManager);
+            Display_Manager.updateInventory(_ResourceManager);
             alert("here 3");
             break;
         default:
