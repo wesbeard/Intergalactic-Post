c4c41e848dd74523e20f8b383bcce80a2b67f619
AdamHasTheBeans <adam.currier@mymail.champlain.edu>
2021-03-12 10:52:09 -0500

    added new buttons and functionality for presses

--- Scripts/Crash-site.js
+++ Scripts/Crash-site.js
@@ -3,27 +3,32 @@ import {Display_Manager, hideElement, showElement, fadeIn, fadeOut, toggleHideUI
 import {progressLocation} from './scripts.js';
 import {asciiCrash} from "./ASCII-Art.js";
 
+const ButtonTypes = {
+    SCRAP_GATHER : 'scrap-gather',
+    MECHANICAL_GATHER : 'mechanical-gather',
+    WIRE_GATHER : 'wire-gather'
+};
+
 class Crash_Site {
 
     constructor(){
         this.stage = 1;
-        this.DM = new Display_Manager();
     }
 
     loadLocation(fadeDelay = 6000) {
         this.setText();
         this.setEventButtons();
         this.setLocationArtwork();
-        setTimeout(this.DM.fadeInTextDisplay, fadeDelay);
+        setTimeout(Display_Manager.fadeInTextDisplay, fadeDelay);
         this.stage++;
     }
     
     progress() {
         
-        this.DM.clearTextDisplay();
+        Display_Manager.clearTextDisplay();
         this.setText();
         this.setEventButtons();
-        this.DM.fadeInTextDisplay();
+        Display_Manager.fadeInTextDisplay();
 
         switch (this.stage) {
             case 4:
@@ -42,57 +47,57 @@ class Crash_Site {
     setText() {
         switch (this.stage) {
             case 1:
-                this.DM.addTextItem("The warning alarm is blairing in your ear", true);
-                this.DM.addTextItem("Please, shut up");
-                this.DM.addTextItem("Red lights blink accross the control board", true);
-                this.DM.addTextItem("That's not good");
-                this.DM.addTextItem("\"Ship integrity: critical\"", true);
-                this.DM.addTextItem("\"Vitals: critical\"", true);
-                this.DM.addTextItem("\"Chance of survival: low\"", true);
-                this.DM.addTextItem("That was the ship's computer, SAL");
-                this.DM.addTextItem("He wasn't helping");
-                this.DM.addTextItem("My body feels like it's been hit by a star freighter");
-                this.DM.addTextItem("Suddenly, you come to your senses", true);
-                this.DM.addTextItem("Oh god, the mail");
-                this.DM.addTextItem("I need to deliver the mail");
+                Display_Manager.addTextItem("The warning alarm is blairing in your ear", true);
+                Display_Manager.addTextItem("Please, shut up");
+                Display_Manager.addTextItem("Red lights blink accross the control board", true);
+                Display_Manager.addTextItem("That's not good");
+                Display_Manager.addTextItem("\"Ship integrity: critical\"", true);
+                Display_Manager.addTextItem("\"Vitals: critical\"", true);
+                Display_Manager.addTextItem("\"Chance of survival: low\"", true);
+                Display_Manager.addTextItem("That was the ship's computer, SAL");
+                Display_Manager.addTextItem("He wasn't helping");
+                Display_Manager.addTextItem("My body feels like it's been hit by a star freighter");
+                Display_Manager.addTextItem("Suddenly, you come to your senses", true);
+                Display_Manager.addTextItem("Oh god, the mail");
+                Display_Manager.addTextItem("I need to deliver the mail");
                 break;
             case 2:
-                this.DM.addTextItem("Where is it?");
-                this.DM.addTextItem("Is it still in the ship?");
-                this.DM.addTextItem("Or scattered all over the whole damn planet?");
-                this.DM.addTextItem("Your concentration is broken by the voice of SAL", true);
-                this.DM.addTextItem("God, does he have an annoying voice");
-                this.DM.addTextItem("\"Captain, if vitals continue to drop");
-                this.DM.addTextItem("you won't even be alive to deliver the mail\"");
-                this.DM.addTextItem("Always the optimist, SAL");
-                this.DM.addTextItem("\"I suggest you locate the oxygenator");
-                this.DM.addTextItem("as well as the hydrolyzer for water generation,");
-                this.DM.addTextItem("they are essential to your survival");
-                this.DM.addTextItem("and may need to be repaired\"");
-                this.DM.addTextItem("I'm screwed", true);
+                Display_Manager.addTextItem("Where is it?");
+                Display_Manager.addTextItem("Is it still in the ship?");
+                Display_Manager.addTextItem("Or scattered all over the whole damn planet?");
+                Display_Manager.addTextItem("Your concentration is broken by the voice of SAL", true);
+                Display_Manager.addTextItem("God, does he have an annoying voice");
+                Display_Manager.addTextItem("\"Captain, if vitals continue to drop");
+                Display_Manager.addTextItem("you won't even be alive to deliver the mail\"");
+                Display_Manager.addTextItem("Always the optimist, SAL");
+                Display_Manager.addTextItem("\"I suggest you locate the oxygenator");
+                Display_Manager.addTextItem("as well as the hydrolyzer for water generation,");
+                Display_Manager.addTextItem("they are essential to your survival");
+                Display_Manager.addTextItem("and may need to be repaired\"");
+                Display_Manager.addTextItem("I'm screwed", true);
                 break;
             case 3:
-                this.DM.addTextItem("The back of the ship is a wreck");
-                this.DM.addTextItem("I have no idea how I'll find anything");
-                this.DM.addTextItem("The good news is that there are piles of letters...");
-                this.DM.addTextItem("So at least some of the mail survived");
-                this.DM.addTextItem("Thank god");
-                this.DM.addTextItem("Heading to the starboard cabin you locate the oxygenator", true);
-                this.DM.addTextItem("It's working, but very slowly", true);
-                this.DM.addTextItem("Should be enough to keep me alive for another few hours at least...");
-                this.DM.addTextItem("On the opposite side of the ship you find the hydrolyzer", true);
-                this.DM.addTextItem("Oh no");
-                this.DM.addTextItem("It may be functional but the tank was ruptured in the crash");
-                this.DM.addTextItem("That's why it's so damn wet back here", true);
-                this.DM.addTextItem("The leak will need to be patched before I can see if it's operational");
-                this.DM.addTextItem("But at least I've got an idea of when I'll die");
-                this.DM.addTextItem("It's a little later than I thought");
+                Display_Manager.addTextItem("The back of the ship is a wreck");
+                Display_Manager.addTextItem("I have no idea how I'll find anything");
+                Display_Manager.addTextItem("The good news is that there are piles of letters...");
+                Display_Manager.addTextItem("So at least some of the mail survived");
+                Display_Manager.addTextItem("Thank god");
+                Display_Manager.addTextItem("Heading to the starboard cabin you locate the oxygenator", true);
+                Display_Manager.addTextItem("It's working, but very slowly", true);
+                Display_Manager.addTextItem("Should be enough to keep me alive for another few hours at least...");
+                Display_Manager.addTextItem("On the opposite side of the ship you find the hydrolyzer", true);
+                Display_Manager.addTextItem("Oh no");
+                Display_Manager.addTextItem("It may be functional but the tank was ruptured in the crash");
+                Display_Manager.addTextItem("That's why it's so damn wet back here", true);
+                Display_Manager.addTextItem("The leak will need to be patched before I can see if it's operational");
+                Display_Manager.addTextItem("But at least I've got an idea of when I'll die");
+                Display_Manager.addTextItem("It's a little later than I thought");
                 break;
             case 4:
-                this.DM.addTextItem("You find automation robots");
+                Display_Manager.addTextItem("You find automation robots");
                 break;
             case 5:
-                this.DM.addTextItem("!WIP! automation gameplay starts now");
+                Display_Manager.addTextItem("!WIP! automation gameplay starts now");
                 break;
         }
     }
@@ -101,25 +106,43 @@ class Crash_Site {
         var button;
         switch (this.stage) {
             case 1:
-                button = this.DM.addEventButton("Look for the mail");
+                button = Display_Manager.addEventButton("Look for the mail");
+                button.addEventListener("click", progressLocation, false);
                 break;
             case 2:
-                button = this.DM.addEventButton("Assess life support");
+                button = Display_Manager.addEventButton("Assess life support");
+                button.addEventListener("click", progressLocation, false);
                 break;
             case 3:
-                button = this.DM.addEventButton("Find a solution");
+                button = Display_Manager.addEventButton("Find a solution");
+                button.addEventListener("click", progressLocation, false);
                 break;
             case 4:
-                button = this.DM.addEventButton("Start automating");
+                button = Display_Manager.addEventButton("Start automating");
+                button.addEventListener("click", progressLocation, false);
                 break;
-        }
-        if (button != undefined) {
-            button.addEventListener("click", progressLocation, false);
+            case 5:
+                button = Display_Manager.addButtonsButton("Gather Scrap", ButtonTypes.SCRAP_GATHER);
+                button.addEventListener("click", this.buttonsPressed, false);
+
+                button = Display_Manager.addButtonsButton("Gather Wires", ButtonTypes.WIRE_GATHER);
+                button.addEventListener("click", this.buttonsPressed, false);
+                
+                button = Display_Manager.addButtonsButton("Gather Mechanical Parts", ButtonTypes.MECHANICAL_GATHER);
+                button.addEventListener("click", this.buttonsPressed, false);
+                
+                break;
+
         }
     }
 
+    buttonsPressed(value){
+        var id = value.target.id;
+        console.log(id);
+    }
+
     setLocationArtwork() {
-        this.DM.setArtwork(asciiCrash)
+        Display_Manager.setArtwork(asciiCrash)
     }
 }
 
